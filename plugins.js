79a58ed329c9762862492b92921f11588f40ba15
Minecraft 2613 <1987sakshamsingh@gmail.com>
2025-06-26 15:35:13 +0530

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,1 +1,26 @@
-const plugins = [];
+const plugins = [
+  {
+    name: "LuckPerms",
+    link: "https://modrinth.com/plugin/luckperms",
+    desc: "Advanced permissions plugin.",
+    youtube: "https://www.youtube.com/watch?v=1xXrPXGvOJw"
+  },
+  {
+    name: "ViaVersion",
+    link: "https://modrinth.com/plugin/viaversion",
+    desc: "Allows cross-version compatibility.",
+    youtube: "" // no tutorial
+  },
+  {
+    name: "EssentialsX",
+    link: "https://essentialsx.net/",
+    desc: "Core commands plugin.",
+    youtube: "https://www.youtube.com/watch?v=UI0qI-0bUuY"
+  },
+  {
+    name: "CoreProtect",
+    link: "https://modrinth.com/plugin/coreprotect",
+    desc: "Block logging & rollback.",
+    youtube: null // also means not available
+  }
+];
